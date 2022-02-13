96f915fd5aa5e0cae3654e8e14898e6457de5085
userb3033 <99432460+userb3033@users.noreply.github.com>
2022-02-12 23:07:36 -0500

    Add files via upload

---
+++ stop-working.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#IMPORTANT:  Once working-better.sh is running, you won't be able to run this by opening a terminal because
+#the mouse and window focus will be all over the place.  So you need to follow the instructions in the
+#README for setting up a Shortcut Key.  I use the Windows key.  One key press will trigger this script and
+#stop working-better.sh, returning your computer to not being possessed.  
+
+killall -9 working-better.sh
+
+#Please send a bit of thanks my way at https://buymeacoffee.com/userb3033  You're awesome!
